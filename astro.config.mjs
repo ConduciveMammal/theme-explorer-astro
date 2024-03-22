ba1419a448d698fa00a3b51c2743113d5a23540d
Liam Merlyn <l.merlyn@hotmail.co.uk>
2024-03-22 19:15:40 +0000

    Updated content

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://merlyndesignworks.co.uk',
+	site: 'https://theme-explorer.merlyndesignworks.co.uk',
 });
--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SiteTitle = 'Theme Explorer';
-export const SiteDescription = 'Welcome to my website!';
+export const SiteDescription = 'Chrome/Firefox Extension for viewing critical details about a Shopify Store';
