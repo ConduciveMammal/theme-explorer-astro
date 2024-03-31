24fe0e8780f621439b41ba16a1fe4cc5aabc2fd1
Liam Merlyn <l.merlyn@hotmail.co.uk>
2024-03-31 20:34:26 +0100

    Updated changelog

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://theme-explorer.merlyndesignworks.co.uk',
-	compressHTML: !import.meta.env.DEV
+  site: 'https://theme-explorer.merlyndesignworks.co.uk',
+  compressHTML: !import.meta.env.DEV,
+  integrations: [mdx()]
 });
