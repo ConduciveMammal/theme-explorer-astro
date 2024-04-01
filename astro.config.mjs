394d578fa6894ff0a6bd4cf67f129f1129ada24c
Liam Merlyn <l.merlyn@hotmail.co.uk>
2024-04-01 12:15:58 +0100

    Added analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 import { defineConfig } from 'astro/config';
 
 import mdx from "@astrojs/mdx";
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://theme-explorer.merlyndesignworks.co.uk',
+  output: 'server',
   compressHTML: !import.meta.env.DEV,
+  adapter: vercel({
+    webAnalytics: { enabled: true }
+  }),
   integrations: [mdx()]
 });
